6e1df427d39de29dc07ae26e85b4aff89675dd9e
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-11 16:24:38 -0300

    Production backend url

--- src/constants/api-url.js
+++ src/constants/api-url.js
@@ -1,4 +1,4 @@
 // const apiUrl = 'http://localhost:8081/api/';
-const apiUrl = 'https://bixoquest.icmc.usp.br/dev/api/';
+const apiUrl = 'https://bixoquest.icmc.usp.br/api/';
 
 export default apiUrl;
